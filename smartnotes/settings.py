e2319029790234a3c3f076e249c6d29e2a72ff3b
Moloko Cordei Mokubedi <molavovic@gmail.com>
2022-08-05 12:47:49 +0200

    key safe

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+rgej8w7-x$jipk8qi8+o^)4b0c=ntudy@ilqqd!pf5c89ovw^'
+#SECRET_KEY = 'django-insecure-+rgej8w7-x$jipk8qi8+o^)4b0c=ntudy@ilqqd!pf5c89ovw^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
